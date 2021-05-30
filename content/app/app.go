1328e9c781d3f5788401b146f2aeac57312b8d8f
Joe Davidson <joe.davidson@infinityworks.com>
2021-05-30 14:52:11 +0100

    Nav

--- content/app/app.go
+++ content/app/app.go
@@ -10,16 +10,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		fmt.Println(r.RequestURI)
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
-		next.ServeHTTP(w, r)
-		log.Println(r.RequestURI)
-	})
-}
-
 func Run(env string) {
 	var c types.Config
 
@@ -44,8 +34,6 @@ func Run(env string) {
 	r.
 		PathPrefix("/").
 		Handler(http.StripPrefix("/content/", http.FileServer(http.Dir("./public/"))))
-
-	r.Use(loggingMiddleware)
 
 	err := http.ListenAndServe(c.Listen, r)
 
--- content/go.mod
+++ content/go.mod
@@ -3,6 +3,6 @@ module content
 go 1.16
 
 require (
-    github.com/gorilla/mux v1.8.0 // indirect
-	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/BurntSushi/toml v0.3.1
+	github.com/gorilla/mux v1.8.0
 )
