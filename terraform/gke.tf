4002fe3864460eb810d97e142fd59bfb25779fef
Davidson <Joe.Davidson@ipfin.co.uk>
2021-06-10 14:32:44 +0100

    Remove gke for now

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -3,18 +3,18 @@ resource "google_service_account" "default" {
   display_name = "Service Account"
 }
 
-resource "google_container_cluster" "primary" {
-  name               = "gfm-cluster"
-  initial_node_count = 3
-  node_config {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-}
+# resource "google_container_cluster" "primary" {
+#   name               = "gfm-cluster"
+#   initial_node_count = 3
+#   node_config {
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#     service_account = google_service_account.default.email
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/cloud-platform"
+#     ]
+#   }
+#   timeouts {
+#     create = "30m"
+#     update = "40m"
+#   }
+# }
