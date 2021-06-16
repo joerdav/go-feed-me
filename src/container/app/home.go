b4fb946f500667337e6674e6ee4faa5defbccf72
Davidson <Joe.Davidson@ipfin.co.uk>
2021-06-16 16:45:38 +0100

    Simplify container

--- src/container/app/home.go
+++ src/container/app/home.go
@@ -3,52 +3,27 @@ package app
 import (
 	"container/templates"
 	"container/types"
-	"context"
 	"net/http"
-
-	"github.com/joe-davidson1802/hotwirehandler"
 )
-
-type ContainerModel struct {
-	url string
-}
-
-func (m ContainerModel) ModelName() string { return "ContainerModel" }
 
 type HomeHandler struct {
 	Config types.Config
 }
 
-func (h HomeHandler) CanHandleModel(m string) bool {
-	return m == ContainerModel{}.ModelName()
-}
-
-func (h HomeHandler) HandleRequest(w http.ResponseWriter, r *http.Request) (error, hotwirehandler.Model) {
-
-	w.Header().Add("Cache-Control", "no-cache")
-	return nil, ContainerModel{
-		url: r.RequestURI,
-	}
-}
-
-func (h HomeHandler) RenderPage(ctx context.Context, m hotwirehandler.Model, w http.ResponseWriter) error {
-	cm := m.(ContainerModel)
-
+func (h HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 
 	var url string
-	if cm.url == "/" || cm.url == "" {
+	if r.RequestURI == "/" || r.RequestURI == "" {
 		url = "/apps" + h.Config.GetDefaultApp().Url
 	} else {
-		url = "/apps" + cm.url
+		url = "/apps" + r.RequestURI
 	}
 
 	t := templates.LayoutTemplate(url, h.Config)
-	err := t.Render(ctx, w)
-	return err
-}
+	err := t.Render(r.Context(), w)
 
-func (h HomeHandler) RenderStream(ctx context.Context, m hotwirehandler.Model, w http.ResponseWriter) error {
-	err := templates.NavTemplate(h.Config.Apps).Render(ctx, w)
-	return err
+	if err != nil {
+		panic(err)
+	}
 }
